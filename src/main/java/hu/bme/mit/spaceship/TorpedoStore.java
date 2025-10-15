ed0690d309c3a56fafead1e45f9b34645bea7cb8
unknown <189207@bme.hu>
2025-10-15 16:53:38 +0200

    Moved random generator to class's variables

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -13,7 +13,10 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
-
+    
+  // simulate random overheating of the launcher bay which prevents firing
+  Random generator = new Random();
+  
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
@@ -35,8 +38,7 @@ public class TorpedoStore {
 
     boolean success = false;
 
-    // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
